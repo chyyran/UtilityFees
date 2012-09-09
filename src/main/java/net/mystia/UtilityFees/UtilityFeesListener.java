ae96e0a772572354d0995cdbc6ab9eb6ba265698
ron975 <ronny6993@gmail.com>
2012-09-09 17:21:33 -0400

    added some basic HashMap saving utils and stuff :)

--- src/main/java/net/mystia/UtilityFees/UtilityFeesListener.java
+++ src/main/java/net/mystia/UtilityFees/UtilityFeesListener.java
@@ -1,5 +1,7 @@
 package net.mystia.UtilityFees;
 
+import java.util.HashMap;
+
 import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -8,13 +10,15 @@ import org.bukkit.event.player.PlayerInteractEvent;
 
 public class UtilityFeesListener implements Listener
 {
+	
+HashMap<String, Integer> counter = new HashMap<String, Integer>();
+	
 @EventHandler(priority = EventPriority.MONITOR)
 public void LeverClickEvent(PlayerInteractEvent e){
 	if (e.getClickedBlock().getType() == Material.LEVER){
-		
-				 
-		String player = event.getPlayer().getName();
-		int newValue = counter.get(player)+1;
+		 
+		String player = e.getPlayer().getName();
+		Integer newValue = counter.get(player)+1;
 		counter.put(player, newValue);
 	}
 }
--- src/main/java/net/mystia/UtilityFees/UtilityFeesMain.java
+++ src/main/java/net/mystia/UtilityFees/UtilityFeesMain.java
@@ -1,11 +1,40 @@
 package net.mystia.UtilityFees;
 
+import net.mystia.UtilityFees.UtilityFeesListener;
+import net.mystia.UtilityFees.SLAPI;
+
 import org.bukkit.plugin.java.JavaPlugin;
+
 public class UtilityFeesMain extends JavaPlugin
 {
 
-	public void onEnable(){
-		
-		HashMap<String, int> counter = new HashMap<String, int>();
+	private UtilityFeesListener listener;
+	private SLAPI SLAPI;
+
+	@SuppressWarnings("static-access") public void onEnable()
+	{
+		try
+		{
+			listener.counter = SLAPI.load("fees.dat");
+		}
+		catch (Exception e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
+
+	@SuppressWarnings("static-access") public void onDisable()
+	{
+		try
+		{
+			SLAPI.save(listener.counter, "fees.dat");
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+
 }
