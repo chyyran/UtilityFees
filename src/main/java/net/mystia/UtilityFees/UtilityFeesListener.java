22f75bbf2a94612b53f76eb853782362f70b0920
ron975 <ronny6993@gmail.com>
2012-09-10 19:25:59 -0400

    Properly implemented SLAPI, added Vault as a dependency.

--- src/main/java/net/mystia/UtilityFees/UtilityFeesListener.java
+++ src/main/java/net/mystia/UtilityFees/UtilityFeesListener.java
@@ -6,22 +6,50 @@ import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 
 public class UtilityFeesListener implements Listener
 {
-	
-HashMap<String, Integer> counter = new HashMap<String, Integer>();
-	
-@EventHandler(priority = EventPriority.MONITOR)
-public void LeverClickEvent(PlayerInteractEvent e){
-	if (e.getClickedBlock().getType() == Material.LEVER){
-		 
-		String player = e.getPlayer().getName();
-		Integer newValue = counter.get(player)+1;
-		counter.put(player, newValue);
-	}
-}
 
+	HashMap<String, Integer> counter = new HashMap<String, Integer>();
+
+	@EventHandler(priority = EventPriority.MONITOR)
+	public void TriggerClickEvent(PlayerInteractEvent e)
+	{
+		if (e.getClickedBlock().getType() == Material.LEVER
+				|| e.getClickedBlock().getType() == Material.STONE_BUTTON)
+		{
+
+			String player = e.getPlayer().getName();
+			Integer newValue = counter.get(player) + 1;
+			counter.put(player, newValue);
+		}
+	}
+
+	public void RedstoneBlockPlaceEvent(BlockPlaceEvent e)
+	{
+		if (e.getBlock().getType() == Material.REDSTONE_TORCH_OFF
+				|| e.getBlock().getType() == Material.WOOD_PLATE
+				|| e.getBlock().getType() == Material.STONE_PLATE)
+		{
+			String player = e.getPlayer().getName();
+			Integer newValue = counter.get(player) + 1;
+			counter.put(player, newValue);
+		}
+	}
+
+	public void RedstoneBlockBreakEvent(BlockBreakEvent e)
+	{
+		if (e.getBlock().getType() == Material.REDSTONE_TORCH_OFF
+				|| e.getBlock().getType() == Material.WOOD_PLATE
+				|| e.getBlock().getType() == Material.STONE_PLATE)
+		{
+			String player = e.getPlayer().getName();
+			Integer newValue = counter.get(player) - 1;
+			counter.put(player, newValue);
+		}
+	}
 
 }
--- src/main/java/net/mystia/UtilityFees/UtilityFeesMain.java
+++ src/main/java/net/mystia/UtilityFees/UtilityFeesMain.java
@@ -1,21 +1,30 @@
 package net.mystia.UtilityFees;
 
-import net.mystia.UtilityFees.UtilityFeesListener;
-import net.mystia.UtilityFees.SLAPI;
+import java.io.File;
 
+import net.milkbowl.vault.economy.Economy;
+import net.mystia.UtilityFees.SLAPI;
+import net.mystia.UtilityFees.UtilityFeesListener;
+
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class UtilityFeesMain extends JavaPlugin
 {
-
+	public static Economy economy = null;
 	private UtilityFeesListener listener;
-	private SLAPI SLAPI;
 
-	@SuppressWarnings("static-access") public void onEnable()
+	public void onEnable()
 	{
+		if (!setupEconomy())
+		{
+			getLogger().severe("No Vault dependency found, disabling plugin");
+			getServer().getPluginManager().disablePlugin(this);
+			return;
+		}
 		try
 		{
-			listener.counter = SLAPI.load("fees.dat");
+			listener.counter = SLAPI.load(this.getDataFolder() + File.separator + "fees.dat");
 		}
 		catch (Exception e)
 		{
@@ -25,11 +34,23 @@ public class UtilityFeesMain extends JavaPlugin
 
 	}
 
-	@SuppressWarnings("static-access") public void onDisable()
+	private boolean setupEconomy()
+	{
+		RegisteredServiceProvider<Economy> economyProvider = getServer().getServicesManager()
+				.getRegistration(net.milkbowl.vault.economy.Economy.class);
+		if (economyProvider != null)
+		{
+			economy = economyProvider.getProvider();
+		}
+
+		return (economy != null);
+	}
+
+	public void onDisable()
 	{
 		try
 		{
-			SLAPI.save(listener.counter, "fees.dat");
+			SLAPI.save(listener.counter, this.getDataFolder() + File.separator + "fees.dat");
 		}
 		catch (Exception e)
 		{
